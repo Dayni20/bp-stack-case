075773ae1b054b48fd97e2da83d1d8550af728c7
Dayni20 <dayniliss@hotmail.com>
2025-09-28 16:51:11 -0500

    Front and back microservices for the client endpoint

--- src/main/java/com/bank/msa/bs/account/customer/api/infrastructure/output/repository/AccountJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.msa.bs.account.customer.api.infrastructure.output.repository;
-
-import com.bank.msa.bs.account.customer.api.infrastructure.output.repository.entity.AccountTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountJpaRepository extends JpaRepository<AccountTable, String> {
-}
